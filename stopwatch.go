d9b23f2ffa4e209300ab289668d59476555dfc7c
xxxb-g <xxxbGamer@proton.me>
2026-02-26 21:53:15 +0100

    the program

---
+++ stopwatch.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"golang.org/x/term"
+)
+
+func main() {
+	start := time.Now()
+	running := true
+
+	// Terminal in Raw Mode setzen
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	// Kanal für Signale
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	// Strg+C-Goroutine
+	go func() {
+		<-sigChan
+		running = false
+	}()
+
+	// Tastendruck-Goroutine (jede Taste!)
+	go func() {
+		buf := make([]byte, 1)
+		for {
+			_, err := os.Stdin.Read(buf)
+			if err == nil {
+				running = false
+				return
+			}
+		}
+	}()
+
+	fmt.Println("Starte Stopwatch. Drücke eine beliebige Taste zum Beenden.")
+
+	for running == true {
+		if running == true {
+			fmt.Printf("\rElapsed time: %s", time.Since(start))
+		}
+		if running == false {
+			time.Sleep(0)
+			fmt.Printf("\rElapsed time: %s", time.Since(start))
+			term.Restore(int(os.Stdin.Fd()), oldState)
+			fmt.Println()
+		}
+	}
+}
